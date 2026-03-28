e40bab5b45e6c5aa2badb6aed6860bbb6c32f2f1
jack2148 <yeachan4842@gmail.com>
2026-03-28 14:18:54 +0900

    complete driving : have to make the final map

--- ws_robot/src/robot_base/launch/nav2.launch.py
+++ ws_robot/src/robot_base/launch/nav2.launch.py
@@ -8,12 +8,7 @@ from launch_ros.actions import SetRemap
 def generate_launch_description():
     pkg_share = get_package_share_directory('robot_base')
     nav2_params_file = os.path.join(pkg_share, 'config', 'nav2_params.yaml')
-<<<<<<< HEAD
-    map_file = os.path.join(os.path.expanduser('~'),
-                            'ws_robot/src/robot base/maps/my_map.yaml')   # 실제 파일명에 맞게
-=======
     map_file = os.path.join(pkg_share, 'maps', 'my_map.yaml')   # 실제 파일명에 맞게
->>>>>>> 0ead885 (change prams for queue error : fix scan range , odom_topic:diff~/odom)
 
     nav2_launch = os.path.join(
         get_package_share_directory('nav2_bringup'),
@@ -32,4 +27,5 @@ def generate_launch_description():
                 'map': map_file
                 }.items()
         )
+
     ])
